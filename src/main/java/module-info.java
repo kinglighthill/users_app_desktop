8664d504b7344c52207f57ebf7262565f786aac8
Kingsley <kingholyhill@gmail.com>
2021-09-20 11:14:12 +0100

    changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.scholarly.utme {
     requires javafx.web;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+//    requires com.dlsc.formsfx;
     requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
