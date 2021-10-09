472e4b84cb524737402c924fabe231930210bcae
KenechiOrjiako <kenechi50@gmail.com>
2021-10-09 15:55:31 +0100

    Created new branch for user authentication flow

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module com.scholarly.utme {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
+//    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
