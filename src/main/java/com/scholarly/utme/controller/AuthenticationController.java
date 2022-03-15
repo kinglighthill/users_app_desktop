6b721fbfe58f4e0e219ba530195de23dcfd608c7
princejohnnie <princejohnc35@gmail.com>
2022-03-15 11:36:55 +0100

    Implemented collecting valid user data for sign up

--- src/main/java/com/scholarly/utme/controller/AuthenticationController.java
+++ src/main/java/com/scholarly/utme/controller/AuthenticationController.java
@@ -224,13 +224,12 @@ public class AuthenticationController implements FxmlView<AuthenticationVM>, Ini
      */
     private void showSignUpUI() {
         Label headerLabel = new Label("Sign Up");
-        Button backButton = new Button("Back");
 
         HBox topBar = new HBox();
         topBar.setSpacing(15);
         topBar.setAlignment(Pos.CENTER_LEFT);
 
-        topBar.getChildren().addAll(backButton, headerLabel);
+        topBar.getChildren().addAll(headerLabel);
 
         VBox.setMargin(topBar, new Insets(40, 0, 0, 20));
 
@@ -262,13 +261,23 @@ public class AuthenticationController implements FxmlView<AuthenticationVM>, Ini
         signupInfoLabel = new Label();
         VBox.setMargin(signupInfoLabel, new Insets(20, 0, 0, 20));
 
-        Button signUpButton = new Button("Sign Up");
-        VBox.setMargin(signUpButton, new Insets(40, 0, 0, 20));
+        HBox backAndSignupButton = new HBox();
+        VBox.setMargin(backAndSignupButton, new Insets(40, 0, 0, 20));
 
+        Button backButton = new Button("Back");
+        HBox.setMargin(backButton, new Insets(0, 10, 0, 0));
+
+        Button signUpButton = new Button("Sign Up");
+        HBox.setMargin(signUpButton, new Insets(0, 0, 0, 10));
+
+        backAndSignupButton.getChildren().clear();
+        backAndSignupButton.getChildren().addAll(backButton, signUpButton);
 
 
         authenticationSection.getChildren().clear();
-        authenticationSection.getChildren().addAll(topBar, firstName, firstNameTextField, lastName, lastNameTextField, phone, phoneTextField, email, emailTextField, passwordLabel, passwordField, signupInfoLabel, signUpButton);
+        authenticationSection.getChildren().addAll(
+                topBar, firstName, firstNameTextField, lastName, lastNameTextField, phone, phoneTextField,
+                email, emailTextField, passwordLabel, passwordField, signupInfoLabel, backAndSignupButton);
 
 
         backButton.setOnAction(e -> {
@@ -287,11 +296,15 @@ public class AuthenticationController implements FxmlView<AuthenticationVM>, Ini
         String email = emailText.getText().trim();
         String password = passwordText.getText().trim();
 
-        if (!firstName.isEmpty() && !lastName.isEmpty() && !phone.isEmpty() && !email.isEmpty() && !password.isEmpty()){
-            User newUser = new User(firstName, lastName, phone, email, password);
-            authenticateUser(newUser);
-            signupInfoLabel.setText("Account created successfully!");
-            showLoginUI();
+        if (!firstName.isEmpty() && !lastName.isEmpty() && !phone.isEmpty() && !password.isEmpty()){
+            if (email.contains("@")){
+                User newUser = new User(firstName, lastName, phone, email, password);
+                authenticateUser(newUser);
+                signupInfoLabel.setText("Account created successfully!");
+            }else {
+                signupInfoLabel.setText("Please input a valid email address");
+            }
+
         }else {
             signupInfoLabel.setText("Kindly fill out all fields");
             signupInfoLabel.setVisible(true);
