b8493adeb488555bfcc000b4ef5529b17e3a5c42
princejohnnie <princejohnc35@gmail.com>
2022-03-15 10:36:03 +0100

    Validate login input

--- src/main/java/com/scholarly/utme/controller/AuthenticationController.java
+++ src/main/java/com/scholarly/utme/controller/AuthenticationController.java
@@ -281,25 +281,6 @@ public class AuthenticationController implements FxmlView<AuthenticationVM>, Ini
         });
     }
 
-   /* private void validateSignupInput(TextField firstNameText, TextField lastNameText, TextField phoneText, TextField emailText, PasswordField passwordText){
-        String firstName = firstNameText.getText().trim();
-        String lastName = lastNameText.getText().trim();
-        String phone = phoneText.getText().trim();
-        String email = emailText.getText().trim();
-        String password = passwordText.getText().trim();
-
-        if (!firstName.isEmpty() && !lastName.isEmpty() && !phone.isEmpty() && !email.isEmpty() && !password.isEmpty()){
-            User newUser = new User(firstName, lastName, phone, email, password);
-            authenticateUser(newUser);
-            signupInfoLabel.setText("Account created successfully!");
-            showLoginUI();
-        }else {
-            signupInfoLabel.setText("Kindly fill out all fields");
-            signupInfoLabel.setVisible(true);
-        }
-
-
-    }*/
     private void showForgotPasswordUI() {
         Label headerLabel = new Label("Forgot Password");
 
