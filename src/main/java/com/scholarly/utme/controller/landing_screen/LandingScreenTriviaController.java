26ffb80932725a901e3ab380230928d344413622
princejohnnie <princejohnc35@gmail.com>
2022-06-15 20:24:31 +0100

    Implemented Trivia UI Welcome Screen

--- src/main/java/com/scholarly/utme/controller/landing_screen/LandingScreenTriviaController.java
+++ src/main/java/com/scholarly/utme/controller/landing_screen/LandingScreenTriviaController.java
@@ -12,19 +12,23 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 import java.net.URL;
-import java.sql.BatchUpdateException;
 import java.util.ResourceBundle;
 
 @FxmlPath("/layouts/landing_screen/landing_screen_trivia.fxml")
 public class LandingScreenTriviaController implements FxmlView<LandingScreenTriviaVM>, Initializable {
 
     @FXML
-    private ImageView centerImage;
+    private ImageView centerImage, prevChallengeIcon1, prevChallengeIcon2, prevChallengeIcon3, playerImage1, playerImage2, playerImage3;
 
     @FXML
     private HBox centerPane;
+
+    @FXML
+    private Panel prevChallenge1, prevChallenge2, prevChallenge3;
 
     @FXML
     private Label challengeHeader, challengeDescription, usernameError, prevHeader;
@@ -42,14 +46,25 @@ public class LandingScreenTriviaController implements FxmlView<LandingScreenTriv
 
         initializeFonts();
 
-        /*challengeHeader.widthProperty().addListener((observable, oldValue, newValue) -> {
-            usernameField.setMaxWidth(newValue.doubleValue()/2);
-        });*/
+
 
     }
 
     private void initializeViews() {
         centerImage.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/young_african_students.jpg").toString()));
+        prevChallengeIcon1.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_icon.png").toString()));
+        prevChallengeIcon2.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_icon.png").toString()));
+        prevChallengeIcon3.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_icon.png").toString()));
+
+        playerImage1.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_player_image.png").toString()));
+        playerImage2.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_player_image.png").toString()));
+        playerImage3.setImage(new Image(getClass().getResource("/drawable/trivia_screen_images/prev_challenge_player_image.png").toString()));
+        Circle clip1 = new Circle(18, 18, 18);
+        playerImage1.setClip(clip1);
+        Circle clip2 = new Circle(18, 18, 18);
+        playerImage2.setClip(clip2);
+        Circle clip3 = new Circle(18, 18, 18);
+        playerImage3.setClip(clip3);
 
     }
 
