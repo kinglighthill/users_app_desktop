f4293c9e144e43ea45686be8f98d0dd863210185
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-15 10:33:04 +0100

    changes

--- src/main/java/com/scholarly/utme/data/util/DbConnection.java
+++ src/main/java/com/scholarly/utme/data/util/DbConnection.java
@@ -1,6 +1,10 @@
 package com.scholarly.utme.data.util;
 
 import com.scholarly.utme.MainApplication;
+import org.sqlite.mc.HmacAlgorithm;
+import org.sqlite.mc.KdfAlgorithm;
+import org.sqlite.mc.SQLiteMCConfig;
+import org.sqlite.mc.SQLiteMCSqlCipherConfig;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,49 +14,21 @@ import java.util.Objects;
 
 public class DbConnection {
     private static final String TAG = "DbConnection: ";
-    private static final String location = MainApplication.class.getResource("/assets/databases/jamb_utme.db").getPath();
-    private static final String DATABASE_URL = "jdbc:sqlite:"+location;
 
     private static Connection connection;
 
-    /*public static Connection getDbConnection() {
-        try {
-            Path mydb = Path.of(System.getenv("LOCALAPPDATA"), "Scholarly JAMB CBT", "jamb_utme.db");
-            final String resName= "/assets/databases/jamb_utme.db";
-            if (!Files.isRegularFile(mydb)) {
-                Files.createDirectories(mydb.getParent());
-                try(var in = DbConnection.class.getResourceAsStream(resName)) {
-                    Objects.requireNonNull(in, () -> "Not found resource: "+resName);
-                    Files.copy(in, mydb);
-                }
-            }
-
-            if (connection == null) {
-                MainApplication.logInfo("Database Connecting..");
-//                connection = DriverManager.getConnection(DATABASE_URL, "", "");
-                connection = DriverManager.getConnection("jdbc:sqlite:"+mydb.toAbsolutePath());
-
-                System.out.println(TAG + "Connection created successfully: " + connection.toString());
-                MainApplication.logInfo(TAG + "Connection created successfully: " + connection.toString());
-            } else {
-                System.out.println(TAG + "Retrieved existing Connection : " + connection);
-                MainApplication.logInfo(TAG + "Retrieved existing Connection : " + connection);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            MainApplication.log(e);
-        }
-        return connection;
-    }*/
-
     public static Connection getDbConnection() {
         try {
-//            var dbFile = Path.of(System.getProperty("user.home"), "Scholarly JAMB CBT", "jamb_utme.db");
+            MainApplication.logInfo("Database Connecting..");
+
             var dbFile = Path.of(System.getenv("LOCALAPPDATA"), "Scholarly JAMB CBT", "jamb_utme.db");
             Files.createDirectories(dbFile.getParent());
             var db = new SQLiteDatabase(dbFile);
 
             connection = db.execute(conn -> conn);
+
+            System.out.println(TAG + "Connection created successfully: " + connection.toString());
+            MainApplication.logInfo(TAG + "Connection created successfully: " + connection.toString());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             MainApplication.log(e);
