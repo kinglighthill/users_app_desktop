fb1dd0c3a712f2c888b7cbb27e107d00f64fc704
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-15 11:53:08 +0100

    bug fixes and performance improvements

--- src/main/java/com/scholarly/utme/data/util/DbConnection.java
+++ src/main/java/com/scholarly/utme/data/util/DbConnection.java
@@ -1,6 +1,7 @@
 package com.scholarly.utme.data.util;
 
 import com.scholarly.utme.MainApplication;
+import com.scholarly.utme.util.Helper;
 import org.sqlite.mc.HmacAlgorithm;
 import org.sqlite.mc.KdfAlgorithm;
 import org.sqlite.mc.SQLiteMCConfig;
@@ -21,7 +22,16 @@ public class DbConnection {
         try {
             MainApplication.logInfo("Database Connecting..");
 
-            var dbFile = Path.of(System.getenv("LOCALAPPDATA"), "Scholarly JAMB CBT", "jamb_utme.db");
+            Path dbFile;
+            if (Helper.isOsType(Helper.OS_TYPE.WIN)) {
+                dbFile = Path.of(System.getenv("LOCALAPPDATA"), "Scholarly JAMB CBT", "jamb_utme.db");
+            } else if (Helper.isOsType(Helper.OS_TYPE.MAC)) {
+                String pathUrl = System.getProperty("user.home") + "/Library/Application Support/";
+                dbFile = Path.of(pathUrl, "Scholarly JAMB CBT", "jamb_utme.db");
+            } else {
+                dbFile = Path.of(System.getProperty("user.home"), "Scholarly JAMB CBT", "jamb_utme.db");
+            }
+
             Files.createDirectories(dbFile.getParent());
             var db = new SQLiteDatabase(dbFile);
 
